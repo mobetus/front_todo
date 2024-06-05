6de6a6ca1d6df8bd19c2e7f71e4e5dcf69c693ed
mobetus <tkfmkim99@naver.com>
2024-06-05 12:24:01 +0900

    modified

--- js/todo.js
+++ js/todo.js
@@ -21,14 +21,12 @@ function renderTodos(todos) {
     todoDiv.classList.add('todo-item');
     todoDiv.textContent = todo.item;
     todosContainer.appendChild(todoDiv);
-    //삭제 버튼 생성 및 이벤트 처리
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('delete-btn');
     deleteBtn.textContent = 'x';
     deleteBtn.addEventListener('click', function () {
       deleteTodo(todo.id);
     });
-    // todoDiv에 삭제 버튼 추가
     todoDiv.appendChild(deleteBtn);
   });
 }
